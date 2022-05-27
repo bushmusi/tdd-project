43811334e7f2453384dc6e05cf6ceb12b4b246fa
bushmusi <bushera.mestofa@gmail.com>
2022-05-27 17:09:34 +0300

    Update with fizzbuzz method

--- solver.rb
+++ solver.rb
@@ -16,4 +16,12 @@ class Solver
     str = get_string
     str.reverse
   end
+
+  def fizzbuzz(number)
+    return 'fizzbuzz' if (number % 3).zero? && (number % 5).zero?
+    return 'fizz' if (number % 3).zero?
+    return 'buzz' if (number % 5).zero?
+
+    number.to_s unless (number % 5).zero? || (number % 3).zero?
+  end
 end
