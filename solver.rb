e54220a3890896d6de9287af2bcf56ec73b46dda
unknown <saied2421998@gmail.com>
2022-05-27 15:02:31 +0200

    pass test of not numerick input after the change

--- solver.rb
+++ solver.rb
@@ -1,7 +1,7 @@
 # Solver class
 class Solver
 	def factorial(number)
-		return 'invalid input' unless number.is_a?(Numeric)
+		raise 'invalid input' unless number.is_a?(Numeric)
 		raise 'invalid number' if number < 0
 		return 1 if number == 0
 		return number * factorial(number-1)  
