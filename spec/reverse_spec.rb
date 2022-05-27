3615e507c2b345d4f141baf2ac1e3c87f97196c5
bushmusi <bushera.mestofa@gmail.com>
2022-05-27 16:35:40 +0300

    Finalise reverse test

--- spec/reverse_spec.rb
+++ spec/reverse_spec.rb
@@ -3,9 +3,9 @@ require_relative '../solver'
 describe 'Reverse' do
   context 'given string' do
     it 'should return cba' do
-      reverse = Solver.new
-      allow(reverse).to receive(:get_string).and_return('abc')
-      expect(reverse.get_revrse).to equal 'cba'
+      instance = Solver.new
+      allow(instance).to receive(:get_string).and_return('abc')
+      expect(instance.reverse).to eq 'cba'
     end
   end
 end
